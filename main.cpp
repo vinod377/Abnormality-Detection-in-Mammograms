a6bd609097a32cd796c09ea9350dced838aab277
vinod <vinod.kumar@tangerine.ai>
2021-05-16 17:50:59 +0530

    updated some comments

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
-//
-//  main.cpp
-//  Breast_cancer_det
-//
-//  Created by Tangerine on 15/04/21.
-//  Copyright © 2021 vinod. All rights reserved.
-//
+/*The code converts .dcm images of DDSM to png format, so that it can be used with
+image processing and Ml algorithms,The main function take two file path
+mypath:downloaded DDSM root folder
+mammoCopyPath:folder path to write the converted images.
+The converted images are named following the same rules as in original data.
+ {mass/calc}_{train/tets}_{full/roi}_{patient_id}_{1/2}
+*/
+
 
 #include <iostream>
 #include "analysing_ddsm_file.h"
 
 int main(int argc, const char * argv[]) {
     
-    const path mypath("/path to downloaded ddsm folder.");
-    std::string  mammoCopyPath = "/path to write the converted .dcm file.";
+    const path mypath("/Users/tangerine/PycharmProjects/dataset/ddsm/");
+    std::string  mammoCopyPath = "/Users/tangerine/PycharmProjects/dataset/ddsm_images_only/";
     ddsmImageConversion ddsm_obj(mypath,mammoCopyPath);
     ddsm_obj.parse_file();
     ddsm_obj.ddsmToPng();
-
     
 }
--- src_code/analysing_ddsm_file.h
+++ src_code/analysing_ddsm_file.h
@@ -1,10 +1,7 @@
-//
-//  analysing_ddsm_file.h
-//  Breast_cancer_det
-//
-//  Created by Tangerine on 17/04/21.
-//  Copyright © 2021 vinod. All rights reserved.
-//
+/*
+ code to read directory containing .dcm file and convert to .png
+ */
+
 
 #ifndef analysing_ddsm_file_h
 #define analysing_ddsm_file_h
@@ -28,7 +25,6 @@ using namespace cv;
 class ddsmImageConversion{
     path mammoSourcePath;
     string mammoCopyPath;
-
     
 public:
     vector<path> path_list;
@@ -39,19 +35,14 @@ public:
     ddsmImageConversion(const path &dir_path,std::string &CopyPath){
         mammoSourcePath=dir_path;
         mammoCopyPath=CopyPath;
-        
     }
-    
-    
 };
 
 void ddsmImageConversion::parse_file()
 {
-
     recursive_directory_iterator end_itr;
     for(recursive_directory_iterator itr(mammoSourcePath);itr!=end_itr;++itr)
     {
-        
         if(itr->path().extension()==".dcm")
         {
             int cnt = 0;
@@ -67,22 +58,18 @@ void ddsmImageConversion::parse_file()
                     boost::split(splited_Calc_Mass,i.c_str(),boost::is_any_of("_,-"));
                 else if(cnt == 12)
                     boost::split(end_name,i.c_str(),boost::is_any_of("."));
-                   
-                                    
             }
             std::string imageName = splited_Calc_Mass[0]+"_"+splited_Calc_Mass[1]+"_"
                                         +splited_full_roi[1]+"_"+splited_full_roi[4]+"_"+end_name[0];
             
             path_list.push_back(itr->path().string());
             dcmImageName.push_back(imageName);
-            cout<<path_list.size();
-        
-
         }
     }
 }
+
 void ddsmImageConversion::ddsmToPng(){
-    std::cout<<"iiii"<<path_list.size()<<std::endl;
+//    std::cout<<"iiii"<<path_list.size()<<std::endl;
     for(int i = 0;i<path_list.size();i++)
     {
         const char * mammoImagePath = path_list[i].c_str();
@@ -92,8 +79,7 @@ void ddsmImageConversion::ddsmToPng(){
             std::cout<<"image is not nUll"<<std::endl;
         
         }
-        
-            //get the information
+        //get the information .dcm image file
         unsigned long nRows = image->getHeight();
         unsigned int nCols = image->getWidth();
         unsigned int nImgs = image->getFrameCount();
@@ -101,17 +87,12 @@ void ddsmImageConversion::ddsmToPng(){
         std::cout<<"Rows number: "<<nRows<<" cols number "<<nCols<<" depth "<<depth<<std::endl;
         Mat mono = Mat(int(image->getHeight()), int(image->getWidth()), CV_16UC1, (Uint16*)(image->getOutputData(16/*bits per sample */)));
         std::string mammo_name = mammoCopyPath+dcmImageName[i]+".png";
-        cout<<"mmmmm"<<mammoCopyPath<<endl;
-        std::cout<<mammo_name<<std::endl;
+        std::cout<<"wrote image"<<'\t'<<mammo_name<<std::endl;
         try{
             imwrite(mammo_name,mono);
-//            imshow("mammo",mono);
-//            int k = waitKey(0);
         }
         catch(...) {};
 
     }
 }
-
-
 #endif /* analysing_ddsm_file_h */
